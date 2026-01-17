d5817f638ca81c967a68f2224347c61bf9e8bd75
enwer <tewbejan@gmail.com>
2026-01-17 14:10:47 +0100

    complete E2E checkout flow

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -2,18 +2,32 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class InventoryPage extends BasePage {
-  readonly titleText: Locator;
-  readonly inventoryItems: Locator;
-  readonly shoppingCart: Locator;
+  readonly firstItemTitle: Locator;
+  readonly firstItemPrice: Locator;
+  readonly firstItemAddToCartButton: Locator;
+  readonly shoppingCartLink: Locator;
+  readonly checkoutButton: Locator;
 
   constructor(page: Page) {
     super(page);
-    this.titleText = page.locator('.title');
-    this.inventoryItems = page.locator('.inventory_item');
-    this.shoppingCart = page.locator('.shopping_cart_link');
+    this.firstItemTitle = page.locator('.inventory_item_name').first();
+    this.firstItemPrice = page.locator('.inventory_item_price').first();
+    this.firstItemAddToCartButton = page.locator('.btn_inventory').first();
+    this.shoppingCartLink = page.locator('.shopping_cart_link');
   }
 
-  async getTitle() {
-    return await this.titleText.textContent();
+  async getFirstItemName() {
+    return await this.firstItemTitle.textContent();
+  }
+  async getFirstItemPrice() {
+    return await this.firstItemPrice.textContent();
+  }
+
+  async addFirstItemToCart() {
+    await this.firstItemAddToCartButton.click();
+  }
+
+  async goToCart() {
+    await this.shoppingCartLink.click();
   }
 }
